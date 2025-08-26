671d151d7c3311461029328e72e93b62933d00a3
sahin Islam <sahinislam878@gmail.com>
2025-08-26 12:39:31 +0530

    Progress: Completed JPA in Job App From Udemy

--- src/main/java/com/sahin/spring_boot_rest/JobRestController.java
+++ src/main/java/com/sahin/spring_boot_rest/JobRestController.java
@@ -43,4 +43,10 @@ public class JobRestController {
         jobService.deleteJob(postId);
         return "Deleted";
     }
+
+    @GetMapping("load")
+    public String load(){
+        jobService.load();
+        return "Success";
+    }
 }
